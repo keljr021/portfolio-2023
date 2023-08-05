e28a9700e87ef89b73c33c5059a0cfe97f049b4e
keljr021 <keljr021@gmail.com>
2023-08-05 16:21:00 -0400

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,16 +4,15 @@
     <v-container fluid class="about">
         <v-row>
             <v-col cols="12" sm="6" md="6" lg="8" class="about-text">
-                <div class="about-text-title">About Me</div>
+                <div class="about-text-title">About</div>
                 <div class="about-text-caption">Art and tech: and my quest to find the median between the two.</div>
                 <div class="about-text-body py-3">
-                    <p>Growing up in the 90's, I was always fascinated with computers, technology, and art. After graduating with a B.S. in Computer Science from UNC Pembroke, I have worked in several IT environments; including hospitals, healthcare, and media companies as well.</p>
-                    <p>My goal is to find an opportunity to flex my creative skills with UI, UX, and visual design; and grow with a great company with like-minded people. My versatility with web development also provides a valuable resource along with my creative skills.</p>
-                    <p>I'm an avid note-taker, researcher, thinker, drawer, and Google...er (if that's a word). When I'm out of work; I'm usually surfing the web, drawing, writing, listening to music, or gaming online.</p>
-                    <p>Want to learn more? Drop a note below!</p>
+                    <p>Computers, technology, and art always fascinated me as a child. That curiosity has never stopped even as an adult today. I'm an avid note-taker, researcher, thinker, drawer, and Google...er (if that's a word).</p>
+                    <p>My goal is to find an opportunity to flex my creative skills with UI, UX, and visual design with a great company with like-minded people. I have previously worked in a variety of IT environments; including hospitals, healthcare, and media companies. </p>
+                    <p>Outside of work; I'm usually surfing the web, drawing, writing, listening to music, or gaming online.</p>
                 </div>
             </v-col>
-            <v-col cols="12" sm="6" md="6" lg="4" class="text-center text-md-right">
+            <v-col cols="12" sm="6" md="6" lg="4" class="text-center my-md-auto">
                 <img class="about-image" alt="about photo" src="../assets/about-alt.jpg" />
             </v-col>
         </v-row>
@@ -33,8 +32,10 @@
 
 .about-text-caption {
     line-height: 2.5;
-    padding: 15px 0;
-    font-size: 20px;
+    padding: 5px 0;
+    font-size: 22px;
+    font-weight: 300;
+    font-style: italic;
 }
 
 .about-text-body {
@@ -46,7 +47,7 @@
 }
 
 .about-image {
-    width: 100%;
+    width: 55%;
 }
 
 @media all and (max-width: 768px) {
