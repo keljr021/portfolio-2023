0eb1d8456c392e8c93e27e1f4f46447ac5189ab5
keljr021 <keljr021@gmail.com>
2023-08-07 22:30:58 -0400

    Aligned work items with work page

--- src/components/partials/WorkList.vue
+++ src/components/partials/WorkList.vue
@@ -4,6 +4,29 @@ export default {
     name: 'WorkList',
     data() {
       return {
+        logos: [
+                {
+                    src: 'logo-to.png'
+                },
+                {
+                    src: 'logo-reys.png'
+                },
+                {
+                    src: 'logo-lb.png',
+                    height: 50
+                },
+                {
+                    src: 'logo-if.png',
+                    height: 50
+                },
+                {
+                    src: 'logo-ka.png',
+                    height: 50
+                },
+                {
+                    src: 'logo-ka-2023.png'
+                }
+            ]
       }
     },
     props: {
@@ -38,17 +61,24 @@ export default {
             </v-col>
             <v-col cols="4">
                 <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
+                    title="Torch Mentor Network" 
+                    caption="A mobile app and website that connects students with mentors" 
+                    :image="imgSrc('to-full.png')"
                     imageWidth="100%" />
             </v-col>
             <v-col cols="4">
                 <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
+                    title="LemonBranch DIY Site" 
+                    caption="A responsive website to search and view tutorials and how-tos" 
+                    :image="imgSrc('lb-full.png')"
                     imageWidth="100%" />
+            </v-col>
+            <v-col cols="4">
+                <work-list-item 
+                    title="Rey's Restaurant App" 
+                    caption="A menu app to streamline the ordering process" 
+                    :image="imgSrc('reys-mock1.png')"
+                    imageHeight="300px" />
             </v-col>
         </v-row>
 
@@ -61,24 +91,38 @@ export default {
         <v-row class="py-8" v-if="showItems === 'all' || showItems === 'frontend'">
             <v-col cols="4">
                 <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
+                    title="iFolio" 
+                    caption="An online portfolio page to host images" 
+                    :image="imgSrc('if-full.png')"
                     imageWidth="100%" />
             </v-col>
             <v-col cols="4">
                 <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
+                    title="ScoreShots 2.0" 
+                    caption="A platform to create and share infographics" 
+                    :image="imgSrc('ss-full.png')"
                     imageWidth="100%" />
             </v-col>
             <v-col cols="4">
                 <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
+                    title="Soccer for Success" 
+                    caption="An e-learning platform for coaches and players" 
+                    :image="imgSrc('s4s-alt.png')"
                     imageWidth="100%" />
+            </v-col>
+            <v-col cols="4">
+                <work-list-item 
+                    title="React Calculator" 
+                    caption="A calculator component built with React JS" 
+                    :image="imgSrc('react-full.png')"
+                    imageHeight="300px" />
+            </v-col>
+            <v-col cols="4">
+                <work-list-item 
+                    title="Vue Calendar" 
+                    caption="A calendar component built with Vue JS" 
+                    :image="imgSrc('vuejs-full.png')"
+                    imageHeight="300px" />
             </v-col>
         </v-row>
 
@@ -89,26 +133,12 @@ export default {
             </v-col>
         </v-row>
         <v-row class="py-8" v-if="showItems === 'all' || showItems === 'logos'">
-            <v-col cols="4">
-                <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
-                    imageWidth="100%" />
-            </v-col>
-            <v-col cols="4">
-                <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
-                    imageWidth="100%" />
-            </v-col>
-            <v-col cols="4">
-                <work-list-item 
-                    title="K&A Morrisey Cleaning Services" 
-                    caption="A responsive website for the family janitorial business" 
-                    :image="imgSrc('ka-full-2023.png')"
-                    imageWidth="100%" />
+            <v-col cols="6" sm="6" md="6" lg="4" v-for="logo in logos">
+                <v-card width="100%" :min-height="$vuetify.display.xs ? 'auto' : 150" class="align-stretch d-flex">
+                    <v-card-text class="align-center ma-auto">
+                        <v-img contain :height="$vuetify.display.xs ? 50 : (logo.height ? logo.height : 80)" :src="imgSrc(logo.src)" />
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
--- src/components/partials/WorkListItem.vue
+++ src/components/partials/WorkListItem.vue
@@ -1,6 +1,11 @@
 <script>
   export default {
     name: 'WorkListItem',
+    data() {
+        return {
+            overlay: false
+        }
+    },
     props: {
         type: String,
         title: String,
@@ -10,27 +15,40 @@
         imageHeight: String,
     },
     methods: {
-        clickItem(input) {
-            console.log('clicked item: ', title);
+        hoverItem(input) {
+            this.overlay = input;
+        },
+
+        clickItem() {
+            console.log('clicked item: ', this.title);
         }
     }
 }
 </script>
 
 <template>
-  <v-card class="work-menu" @click="clickItem">
-    <v-card-text class="text-center">
+  <v-card height="200" @click="clickItem" @mouseover="hoverItem(true)" @mouseleave="hoverItem(false)">
+    <v-card-text class="pa-0 ma-0">
         <v-img :width="imageWidth" :height="imageHeight" :src="image"/>
+
+        <v-overlay
+          v-model="overlay"
+          class="align-center justify-center"
+          scrim="#18184d"
+          contained>
+            <div class='list-item-text text-white text-left pa-3'>
+                <div class="list-item-text-title py-2 font-weight-bold">{{ title }}</div>
+                <div class="list-item-text-caption">{{ caption }}</div>
+            </div>
+        </v-overlay>
+
+       
     </v-card-text>
   </v-card>
 </template>
 
-<style>
-.work-menu-icon {
-  font-size: 28px;
-}
-
-.work-menu-title {
-  font-size: 20px;
+<style scoped lang="scss">
+.list-item-text {
+    width: 300px;
 }
 </style>
