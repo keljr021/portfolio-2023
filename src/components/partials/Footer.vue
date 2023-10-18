4e3330644acd79c264989c436f51de02dda0bd32
keljr021 <keljr021@gmail.com>
2023-10-18 12:17:10 -0400

    Added Dribbble and Behance links to footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -20,20 +20,26 @@ export default {
       <v-spacer></v-spacer>
       <v-col cols="12" class="text-center">
           <span class="footer-link" @click="openWindow('mailto:kmjr07@yahoo.com')" title="Email kmjr07@yahoo.com">
-              <v-icon class="mr-2" icon="mdi-email" variant="text"  />
+              <v-icon class="mr-2 mt-0" icon="mdi-email" variant="text"  />
               <span v-if="!$vuetify.display.xs">kmjr07@yahoo.com</span>
           </span>
-          <span class="footer-link mx-lg-8" @click="openWindow('mailto:keljr021@gmail.com')" title="Email keljr021@gmail.com">
-              <v-icon class="mr-2" icon="mdi-email" variant="text"  />
+          <span class="footer-link mx-lg-8 mb-2" @click="openWindow('mailto:keljr021@gmail.com')" title="Email keljr021@gmail.com">
+              <v-icon class="mr-2 mt-0" icon="mdi-email" variant="text"  />
               <span v-if="!$vuetify.display.xs">keljr021@gmail.com</span>
           </span>
-          <span class="footer-link mx-lg-8" @click="openWindow('https://www.linkedin.com/in/kelvin-morrisey-jr-514360a7/')" title="View LinkedIn Profile">
+          <span class="footer-link mx-lg-8 mb-2" @click="openWindow('https://www.linkedin.com/in/kelvin-morrisey-jr-514360a7/')" title="View LinkedIn Profile">
             <img class="footer-icon" src="../../assets/LI-black.png" alt="LinkedIn link" />
           </span>
-          <span class="footer-link mx-lg-8" @click="openWindow('https://github.com/keljr021')" title="View GitHub Profile">
+          <span class="footer-link mx-lg-8 mb-2" @click="openWindow('https://github.com/keljr021')" title="View GitHub Profile">
             <img class="footer-icon" src="../../assets/github-black.png" alt="GitHub link" />
           </span>
-      </v-col>
+          <span class="footer-link mx-lg-8 mb-2" @click="openWindow('https://dribbble.com/keljr021')" title="View Dribbble Profile">
+            <img class="footer-icon" src="../../assets/dr-logo.png" alt="Dribbble link" />
+          </span>
+          <span class="footer-link mx-lg-8 mb-2" @click="openWindow('https://www.behance.net/kelvin-m')" title="View Behance Profile">
+            <img class="footer-icon" src="../../assets/be-logo-alt2.png" alt="Behance link" />
+          </span>
+        </v-col>
       <v-spacer></v-spacer>
     </v-row>
 </template>
@@ -41,7 +47,7 @@ export default {
 <style scoped lang="scss">
 .footer {
   width: calc(100% - 40px);
-  font-size: 75%;
+  font-size: 95%;
   position: absolute;
   bottom: 0;
   margin: 0;
@@ -68,20 +74,20 @@ export default {
 @media all and (min-width: 768px) and (max-width: 1023px) {
 
   .footer-link {
-    width: initial;
-    margin: 10px 25px;
+    width: calc(25% - 80px);
+    margin: 10px 40px;
     text-align: center;
   }
 }
 
-@media all and (max-width: 768px) {
+@media all and (max-width: 767px) {
 
   .footer-link {
     width: calc(25% - 60px);
     margin-top: 10px;
     margin-bottom: 10px;
-    margin-right: 30px;
-    margin-left: 20px;
+    margin-right: 25px;
+    margin-left: 0;
     text-align: center;
   }
 }
